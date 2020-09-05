0035b3ad110f28ddd52b944ef42b179793929c5a
Chris Cosentino <chriscosentino@hotmail.com>
2020-09-05 01:36:06 -0400

    puppet

--- scraper.js
+++ scraper.js
@@ -1,13 +1,79 @@
 const puppeteer = require('puppeteer');
 const mongoose = require('mongoose');
+const connectDB = require('./config/db');
 
-function sayHello() {
-  console.log('hello');
+const Tracker = require('./models/Tracker');
+
+// function sayHello() {
+//   console.log('hello');
+// }
+
+// sayHello();
+
+async function scrapeProducts() {
+  const browserPromise = puppeteer.launch({
+    headless: false,
+    args: ['--no-sandbox'],
+  });
+
+  console.log('starting scheduler');
+  await connectDB();
+  const trackers = await Tracker.find();
+  console.log(trackers);
+  console.log('found');
+
+  const browser = await browserPromise;
+  const context = await browser.createIncognitoBrowserContext();
+
+  trackers.forEach(async (tracker) => {
+    console.log(tracker.price);
+
+    const page = await context.newPage();
+
+    //dont load css or anything else that would slow the page down
+    await page.setRequestInterception(true);
+    page.on('request', (request) => {
+      if (
+        ['image', 'stylesheet', 'font', 'script'].indexOf(
+          request.resourceType()
+        ) !== -1
+      ) {
+        request.abort();
+      } else {
+        request.continue();
+      }
+    });
+    await page.goto(tracker.url, {
+      waitUntil: 'domcontentloaded',
+      timeout: 200000,
+    });
+
+    const product = await page.evaluate(() => {
+      const priceString = document.getElementById('price_inside_buybox')
+        .innerText;
+
+      let priceNum = priceString.replace(/\D/g, '');
+
+      var output = [
+        priceNum.slice(0, priceNum.length - 2),
+        '.',
+        priceNum.slice(priceNum.length - 2),
+      ].join('');
+
+      const p = {
+        price: parseFloat(output),
+        title: document.getElementById('productTitle').innerText,
+      };
+
+      return p;
+    });
+    console.log(product);
+    await page.close();
+  });
 }
 
-sayHello();
+scrapeProducts();
 
-function scrapeProducts() {}
 // cron.schedule('*/1 * * * *', async () => {
 //   console.log('started cron job');
 //   const trackers = await Tracker.find();
@@ -52,4 +118,4 @@ function scrapeProducts() {}
 //   });
 
 //   console.log('running job every hour');
-// });
+// }

5aed0388e91538247235f261d8ba503274ff1839
Chris Cosentino <chriscosentino@hotmail.com>
2020-09-06 18:39:36 -0400

    ads

--- client/src/components/Ads.js
+++ client/src/components/Ads.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
 const Ads = () => {
+  const style = {
+    border: 'none',
+  };
   return (
-    <div className='ads'>
+    <div
+      style={{
+        position: 'fixed',
+        width: '728',
+        height: '90',
+        bottom: '0',
+        left: '0',
+        backgroundColor: 'red',
+      }}>
       <iframe
         src='//rcm-na.amazon-adsystem.com/e/cm?o=15&p=48&l=ur1&category=amazonhomepage&f=ifr&linkID=507fd0cebea662d7ca6170114384246b&t=owlpiece-20&tracking_id=owlpiece-20'
         width='728'
         height='90'
         scrolling='no'
-        style={{ border: 'none', height: '90', width: '728' }}
+        border='0'
+        marginWidth='0'
+        style={style}
         frameBorder='0'></iframe>
     </div>
   );
